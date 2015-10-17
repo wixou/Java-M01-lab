b23b338fd1859721a93c9032725b8f801596a67e
Walid Amghar <wixouamg@gmail.com>
2015-10-17 12:17:32 +0100

    Update part2.java

--- Lab1/Part2/part2.java
+++ Lab1/Part2/part2.java
@@ -1,29 +1,23 @@
 import java.util.Scanner;
 
-public class part2{
-	
-	public static void main(String[] args){
-		int max=-50000;
-		int min=50000;
+public class Lab1 {
+
+public static void main(String[] args) {
+		
+		double area, circumference, radius;
 		
 		Scanner input = new Scanner(System.in);
-		System.out.println("Please, enter three integer numbers: ");
-		int i1 = input.nextInt();
-		int i2 = input.nextInt();
-		int i3 = input.nextInt();
+		System.out.println("Please entre the value of the diameter !!");
+		double diameter = input.nextDouble();
 		
-		int avg=(i1+i2+i3)/3;
+		radius = diameter / 2;
+		area = Math.PI * Math.pow(radius,2);
+		circumference = 2 * Math.PI * radius;
 		
-		if(i1>max)	max=i1;
-		if(i2>max)	max=i2;
-		if(i3>max)	max=i3;
+		System.out.println("The area of the circle is "+area);
+		System.out.println("The circumference of the circle is "+circumference);
 		
-		if(i1<min)	min=i1;
-		if(i2<min)	min=i2;
-		if(i3<min)	min=i3;
 		
-		System.out.println("The average is: "+avg"\n");
-		System.out.println("The greatest number is: "+max"\n");
-		System.out.println("The smallest number is: "+min"\n");
 	}
+
 }
